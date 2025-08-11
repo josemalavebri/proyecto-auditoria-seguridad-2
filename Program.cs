f6dd78d7f2faded2bd212e9335417a5f3d21cda8
josemalavebri <jose.malavebri@outlook.com>
2025-08-11 14:49:29 -0500

    fin

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<EncuestaDBContext>(options =>
-    options.UseSqlServer("Server=DESKTOP-12H49JM;Database=EncuestaDB10;Trusted_Connection=True;TrustServerCertificate=True;"));
+    options.UseSqlServer("Server=ROBERT;Database=EncuestaDB10;Trusted_Connection=True;TrustServerCertificate=True;"));
 
 
 builder.Services.AddScoped<IRepositoryGet, RepositoryEncuestaEjecucion>();
